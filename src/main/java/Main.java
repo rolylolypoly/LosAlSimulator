8a7b8339f2b0e49b428c6851b39e7deaa06abc32
Will <dangwilliam2@gmail.com>
2015-11-21 10:11:17 -0800

    No.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("============================================ Hello! ============================================");
         System.out.println("============== Welcome to Los Alamitos High School Simulator Version 1 x 10^-72! ===============");
         System.out.println("========By using this software you agree to all the terms, which will be specified later========");
+        System.out.println("=======This software is provided free of charge, please provide bank account information.=======");
         MrSmiths.talk();
     }
 }
