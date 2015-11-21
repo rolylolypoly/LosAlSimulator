3da246695004b5aa2246f71996c6a1cb30c77dd5
Will <dangwilliam2@gmail.com>
2015-11-20 17:12:35 -0800

    Changed the exclamation point to a period

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("============================================ Hello! ============================================");
         System.out.println("============== Welcome to Los Alamitos High School Simulator Version 1 x 10^-72! ===============");
-        System.out.println("==============================================Die!==============================================");
+        System.out.println("==============================================Die.==============================================");
     }
 }
