cbb73f4054db2b376d3b06aca54e36f181e97b22
Will <dangwilliam2@gmail.com>
2015-11-20 17:09:57 -0800

    Initial commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Will on 11/20/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("============================================ Hello! ============================================");
+        System.out.println("============== Welcome to Los Alamitos High School Simulator Version 1 x 10^-72! ===============");
+        System.out.println("==============================================Die!==============================================");
+    }
+}
